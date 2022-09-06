3d7038c4d8534bc247d9f704aacc7d75088232a6
Antoine <antoine.lucsko@wanadoo.fr>
2022-09-06 12:05:18 +0200

    Update server.js

--- semaine_02_approfondissement/05_mongodb_J4/Exercices/inventory/app/server.js
+++ semaine_02_approfondissement/05_mongodb_J4/Exercices/inventory/app/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import router from "./router.js";
+import { connect } from "./model/product.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+const { DB_PASSWORD, DB_NAME, DB_LOGIN, DB_PORT } = process.env;
 
 const app = express();
 const port = 3002;
 
 app.use("/", router);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connect({ DB_PASSWORD, DB_NAME, DB_LOGIN, DB_PORT })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch(console.error);
