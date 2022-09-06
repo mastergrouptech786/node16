da41ffc979f4a25962c35ea10b9596f0ffbe2760
Antoine <antoine.lucsko@wanadoo.fr>
2022-09-06 12:03:48 +0200

    Update router.js

--- semaine_02_approfondissement/05_mongodb_J4/Exercices/inventory/app/router.js
+++ semaine_02_approfondissement/05_mongodb_J4/Exercices/inventory/app/router.js
@@ -1,42 +1,19 @@
 import express from "express";
+import router from "./router.js";
+import { connect } from "./model/product.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+const { DB_PASSWORD, DB_NAME, DB_LOGIN, DB_PORT } = process.env;
 
-import { all, find, remove } from "./model/product.js"
+const app = express();
+const port = 3002;
 
-const router = express.Router();
+app.use("/", router);
 
-/*
-AVEC CURL un client dans la console vous lancez une requête de type GET comme suit, avec un paramètre dynamique
-
-curl http://localhost:3002
-*/
-router.get("/", async (req, res) =>{
-    const products = await all();
-
-    res.json({ products });
-});
-
-/*
-AVEC CURL un client dans la console vous lancez une requête de type GET comme suit, avec un paramètre dynamique
-
-curl http://localhost:3002/delete/Alex
-*/
-router.get("/show/:name", async (req, res) =>{
-    const { name } = req.params;
-    const product = await find(name);
-
-    res.json({ product });
-});
-
-/*
-AVEC CURL un client dans la console vous lancez une requête de type DELETE comme suit 
-
-curl --request "DELETE" http://localhost:3002/delete/Alex
-*/
-router.delete("/delete/:name", async (req, res) =>{
-    const { name } = req.params;
-    const product = await remove(name);
-
-    res.json({ name });
-});
-
-export default router;
+connect({ DB_PASSWORD, DB_NAME, DB_LOGIN, DB_PORT })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch(console.error);
